f566b3f197afacebd24fc2d08b237fc7bead05b8
asloba <aslobaa@mail.ru>
2021-05-20 00:01:56 +0300

    Test modified

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Test data"))
+                .body("data", equalTo("Test dat"))
         ;
     }
 }
