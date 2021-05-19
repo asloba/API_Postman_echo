5daec39720844dd3af4046c2c79936806a16f8ba
asloba <aslobaa@mail.ru>
2021-05-19 23:48:14 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void ShouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Test data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Test data"))
+        ;
+    }
+}
